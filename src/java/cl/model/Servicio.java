d1721f57fb328790172787cda17e146203f04426
sistemas <sistemas@VRE_DOCENTE203.inacap.cl>
2019-04-23 23:21:10 -0300

    Hice el eliminar

--- src/java/cl/model/Servicio.java
+++ src/java/cl/model/Servicio.java
@@ -6,29 +6,28 @@ import javax.ejb.Singleton;
 /**
  *
  * @author profe Hernán
- * 
+ *
  */
 @Singleton
 public class Servicio implements ServicioLocal {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-    
     private ArrayList<Cliente> lista = new ArrayList<>();
 
     public Servicio() {
-        
-        lista.add(new Cliente("1-1","Juan","Cerda"));
-        lista.add(new Cliente("1-2","David","Aguirre"));
-        lista.add(new Cliente("1-3","Jorge","Caceres"));
-        
+
+        lista.add(new Cliente("1-1", "Juan", "Cerda"));
+        lista.add(new Cliente("1-2", "David", "Aguirre"));
+        lista.add(new Cliente("1-3", "Jorge", "Caceres"));
+
     }
 
     @Override
     public void agregarCliente(Cliente cli) {
-        
+
         lista.add(cli);
-        
+
     }
 
     @Override
@@ -45,15 +44,17 @@ public class Servicio implements ServicioLocal {
         }
         return null;
     }
-    
-    
 
-  
-    
-    
-    
-    
-    
-    
-    
+    @Override
+    public String eliminar(String rut) {
+        Cliente cl = buscar(rut);
+
+        if (cl == null) {
+            return "Rut no encontrado";
+        } else {
+            lista.remove(cl);
+            return "cliente eliminado";
+        }
+    }
+
 }
